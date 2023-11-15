e143fb9dfce0c61d2d1c6ba20d2ca98181731196
HeroGamers <hero@herogamers.dev>
2023-11-15 17:25:23 +0800

    Add SpeechBrain stuff

--- server/SpeakerRecognition/SpeakerClass.py
+++ server/SpeakerRecognition/SpeakerClass.py
@@ -1,9 +1,20 @@
+from typing import Optional
+
+
+class Speaker:
+    def __init__(self, name: str):
+        self.name = name
+
+    async def is_speaker(self, audio: str) -> bool:
+        pass
+
+
 class SpeakerClass:
     def __init__(self):
+        self.speakers: list[Speaker] = []
+
+    async def enroll(self, audio: str, name: str) -> Speaker:
         pass
 
-    async def enroll(self, audio):
-        pass
-
-    async def recognize(self, audio):
+    async def recognize(self, audio: str) -> Optional[Speaker]:
         pass
--- server/SpeechRecognition/SpeechBrain.py
+++ server/SpeechRecognition/SpeechBrain.py
@@ -1,12 +1,19 @@
 from SpeechClass import SpeechClass
+from speechbrain.pretrained import EncoderDecoderASR
 
 
 class SpeechBrain(SpeechClass):
-    def __init__(self):
+    def __init__(self, model: str):
         super().__init__()
 
-    async def enroll(self, audio):
-        pass
+        if model.lower() == "wav2vec2":
+            self.asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-wav2vec2-commonvoice-en",
+                                                            savedir="pretrained_models/asr-wav2vec2-commonvoice-en")
+        elif model.lower() == "librispeech":
+            self.asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-transformer-transformerlm-librispeech", savedir="pretrained_models/asr-transformer-transformerlm-librispeech")
+        else:
+            raise ValueError(f"Model {model} not supported")
 
-    async def recognize(self, audio):
-        pass
+    async def get_text(self, audio):
+        transcription = self.asr_model.transcribe_file(audio)
+        return transcription
