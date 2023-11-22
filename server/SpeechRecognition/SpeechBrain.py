1a6ca2a1db91ddea8216905b9b786d6a9547c0dd
Mads Nielsen <madsnielsen@Madss-MacBook-Pro.local>
2023-11-22 15:28:35 +0800

    gitignore now ignores wav files

--- server/SpeechRecognition/SpeechBrain.py
+++ server/SpeechRecognition/SpeechBrain.py
@@ -12,14 +12,16 @@ class SpeechBrain(SpeechClass):
             self.asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-wav2vec2-commonvoice-en",
                                                             savedir="pretrained_models/asr-wav2vec2-commonvoice-en")
         elif model.lower() == "librispeech":
-            self.asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-transformer-transformerlm-librispeech", savedir="pretrained_models/asr-transformer-transformerlm-librispeech")
+            self.asr_model = EncoderDecoderASR.from_hparams(
+                source="speechbrain/asr-transformer-transformerlm-librispeech",
+                savedir="pretrained_models/asr-transformer-transformerlm-librispeech")
         else:
             raise ValueError(f"Model {model} not supported")
 
     async def get_text(self, audio):
         transcription = self.asr_model.transcribe_file(audio)
         return transcription
-    
+
     def preprocess_audio(self, audio_path: str) -> str:
         """
         Applies preprocessing steps to the audio file such as noise reduction, 
@@ -29,8 +31,8 @@ class SpeechBrain(SpeechClass):
         waveform, sample_rate = torchaudio.load(audio_path)
 
         # Setting the low-frequency cutoff and high-frequency cutoff of a bandpass filter
-        low_cutoff_freq = 100 #low-frequency off 100Hz
-        high_cutoff_freq = 8000 #higt-frequency off 8000Hz
+        low_cutoff_freq = 100  # low-frequency off 100Hz
+        high_cutoff_freq = 10000  # higt-frequency off 8000Hz
 
         bandpass_filter = T.BandpassBiquad(sample_rate, low_cutoff_freq, high_cutoff_freq)
 
@@ -39,6 +41,10 @@ class SpeechBrain(SpeechClass):
         # Save the preprocessed audio to a temporary file
         processed_audio_path = "temp_processed_audio.wav"
         torchaudio.save(processed_audio_path, waveform_filtered, sample_rate)
-    
-        
+
         return processed_audio_path
+
+
+if __name__ == '__main__':
+    speech = SpeechBrain("wav2vec2")
+    print(speech.get_text("test.wav"))
